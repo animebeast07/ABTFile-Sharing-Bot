2ca26a86857ba56aa425b9a23611c66d6d43129a
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2025-04-12 19:54:43 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -10,12 +10,8 @@ from pyrogram.errors import FloodWait
 
 
 
-async def is_subscribed(filter, client, update):
+async def is_subscribed1(filter, client, update):
     if not FORCE_SUB_CHANNEL:
-        return True
-    if not FORCE_SUB_CHANNEL2:
-        return True
-    if not FORCE_SUB_CHANNEL3:
         return True
     user_id = update.from_user.id
     if user_id in ADMINS:
@@ -27,22 +23,41 @@ async def is_subscribed(filter, client, update):
 
     if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
         return False
+    else:
+        return True
+
+
+async def is_subscribed2(filter, client, update):
+    if not FORCE_SUB_CHANNEL2:
+        return True
+    user_id = update.from_user.id
+    if user_id in ADMINS:
+        return True
     try:
         member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL2, user_id = user_id)
     except UserNotParticipant:
         return False
-    else:
-        return True
 
     if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
         return False
+    else:
+        return True  
+
+async def is_subscribed3(filter, client, update):
+    if not FORCE_SUB_CHANNEL3:
+        return True
+    user_id = update.from_user.id
+    if user_id in ADMINS:
+        return True
     try:
         member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL3, user_id = user_id)
     except UserNotParticipant:
         return False
+
+    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
+        return False
     else:
         return True
-
 
 async def encode(string):
     string_bytes = string.encode("ascii")
@@ -129,7 +144,9 @@ def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-subscribed = filters.create(is_subscribed)
+subscribed1 = filters.create(is_subscribed1)
+subscribed2 = filters.create(is_subscribed2)
+subscribed3 = filters.create(is_subscribed3)
        
 
 
