38f857a21ddc6e6a2af4fdeae3bec6f2fa8fe163
animebeast07 <narutouzumakitamil2005@gmail.com>
2025-04-12 12:33:24 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -3,7 +3,7 @@ import re
 import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
-from config import FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2,ADMINS
+from config import FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2,FORCE_SUB_CHANNEL3,ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
@@ -14,6 +14,8 @@ async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
         return True
     if not FORCE_SUB_CHANNEL2:
+        return True
+    if not FORCE_SUB_CHANNEL3:
         return True
     user_id = update.from_user.id
     if user_id in ADMINS:
@@ -27,6 +29,15 @@ async def is_subscribed(filter, client, update):
         return False
     try:
         member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL2, user_id = user_id)
+    except UserNotParticipant:
+        return False
+    else:
+        return True
+
+    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
+        return False
+    try:
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL3, user_id = user_id)
     except UserNotParticipant:
         return False
     else:
