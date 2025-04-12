03fd71af082e1a5c54dd9bcdb94454c225d2f248
animebeast07 <narutouzumakitamil2005@gmail.com>
2025-04-12 14:34:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -126,18 +126,26 @@ async def start_command(client: Client, message: Message):
         return
 
     
+    
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    buttons = [ 
-        [ 
-        InlineKeyboardButton(text="Join Channel", url=client.invitelink), 
-        InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
-        InlineKeyboardButton(text="Join Channel", url=client.invitelink3) 
-        ] 
+    buttons = [
+        [
+            InlineKeyboardButton(
+                "Join Channel",
+                url = client.invitelink),
+            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ •", url=client.invitelink2),
+        ]
     ]
-          try: buttons.append( 
-              [ 
-                  InlineKeyboardButton( text = 'Try Again', url = f"https://t.me/{client.username}?start={message.command[1]}" ) ] )
+    try:
+        buttons.append(
+            [
+                InlineKeyboardButton(
+                    text = 'Try Again',
+                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                )
+            ]
+        )
 
 
     except IndexError:
